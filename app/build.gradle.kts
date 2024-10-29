e622a4059db108ebc1d8991136b902e2b766ab6b
Anson Nguyen <an23nguyen44@gmail.com>
2024-10-28 20:56:32 -0500

    Revert "Camera Access"
    This reverts commit be5c6aabe83c80610363800c820d1d5fb2419ec1.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,19 +58,6 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
-
-    // CameraX core library using the camera2 implementation
-    val camerax_version = "1.3.0-alpha02"
-    // The following line is optional, as the core library is included indirectly by camera-camera2
-    implementation("androidx.camera:camera-core:${camerax_version}")
-    implementation("androidx.camera:camera-camera2:${camerax_version}")
-
-    // If you want to additionally use the CameraX Lifecycle library
-    implementation("androidx.camera:camera-lifecycle:${camerax_version}")
-
-    // If you want to additionally use the CameraX View class
-    implementation("androidx.camera:camera-view:${camerax_version}")
-
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
